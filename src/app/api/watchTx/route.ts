db1ee8064f1ed328743fff5eb09204b2c8bc1310
horsefacts <webmaster@terminally.online>
2024-03-05 11:02:20 -0500

    use onceupon image API

--- src/app/api/watchTx/route.ts
+++ src/app/api/watchTx/route.ts
@@ -1,7 +1,4 @@
-import getListing from "@/app/lib/getListing";
-import getToken from "@/app/lib/getToken";
 import { NextRequest, NextResponse } from "next/server";
-import { formatEther } from "viem";
 
 const HOST = process.env["HOST"] ?? "https://floor-sweep-frame.vercel.app";
 
@@ -12,20 +9,37 @@ export async function POST(req: NextRequest) {
   } = frameData;
   const hash = txHash.slice(1, -1);
 
-  const onceUponResponse = await fetch(
-    `https://api.onceupon.gg/v1/transactions/${hash}/farcaster-frame?delay=4000`,
+  let postUrl = `${HOST}/api/watchTx`;
+  let imageUrl = `${HOST}/api/images/watchTx`
+
+  const txData = await fetch(
+    `https://api.onceupon.gg/v1/transactions/${txHash}`,
+  );
+  if (txData.status === 200) {
+    imageUrl = `https://og.onceupon.gg/card/${txHash}?datetime=${Date.now()}`
+  }
+
+  return new NextResponse(
+    `<!DOCTYPE html>
+      <html>
+        <head>
+          <meta property="og:title" content="Floor Sweeper" />
+          <meta property="og:image" content="${imageUrl}" />
+          <meta name="fc:frame" content="vNext" />
+          <meta name="fc:frame:image" content="${imageUrl}" />
+          <meta name="fc:frame:post_url" content="${postUrl}" />
+          <meta name="fc:frame:button:1" content="Refresh" />
+          <meta name="fc:frame:button:2" content="View on BaseScan" />
+          <meta name="fc:frame:button:2:action" content="link" />
+          <meta name="fc:frame:button:2:target" content="https://basescan.org/tx/${hash}" />
+        </head>
+        <body></body>
+      </html>`,
     {
-      method: "POST",
-      body: JSON.stringify(frameData),
+      status: 200,
+      headers: {
+        "Content-Type": "text/html",
+      },
     },
   );
-  const html = await onceUponResponse.text();
-  console.log(html);
-
-  return new NextResponse(html, {
-    status: 200,
-    headers: {
-      "Content-Type": "text/html",
-    },
-  });
 }
