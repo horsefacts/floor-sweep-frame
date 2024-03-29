3fe0d6af0059fb98eb4275922193e4f5d91b4ade
horsefacts <webmaster@terminally.online>
2024-03-28 21:28:04 -0400

    temporarily disable tx

--- src/app/api/tx/[orderHash]/route.ts
+++ src/app/api/tx/[orderHash]/route.ts
@@ -31,11 +31,9 @@ export async function POST(
     chainId: `eip155:${chain}`,
     method: "eth_sendTransaction",
     params: {
-      functionSignature: "", // deprecated, use abi below
-      abi: [functionAbi, ...errorsAbi],
-      to,
-      data,
-      value: value.toString(),
+      //abi: [functionAbi, ...errorsAbi],
+      to: 0x79d31bFcA5Fda7A4F15b36763d2e44C99D811a6C,
+      value: "42069",
       gas: "1000000",
     },
   };
